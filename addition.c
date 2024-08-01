881b6994ff80b3b63276d69a5a8d41edc244e65e
SUROJIT SAHA <surojitsaha2005@gmail.com>
2024-08-01 18:44:52 +0530

    pussing addition code

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int x, y;
+    int sub;
+    printf("Enter the 2 number: \n");
+    scanf("%d%d", &x, &y);
+    sub = x - y;
+    printf("The subtraction is %d ", sub);
+}
